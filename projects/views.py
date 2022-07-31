2eb74cd131e1747fda4238dccc0b4ef075a634b6
Bikash101 <dhakalbikash0@gmail.com>
2022-07-31 16:25:02 +0545

    displaying data from database

--- projects/views.py
+++ projects/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from projects.models import Project
+
+from .models import Project
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello Bikash. ")
+    projects = Project.objects
+    return render(request, 'projects/index.html', {'projects':projects})
 
