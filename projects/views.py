d23b1f9c87a603bfa0c65c8bf69218ced3c77212
Bikash101 <dhakalbikash0@gmail.com>
2022-07-31 18:21:40 +0545

    adding media content and object link

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
+import projects
 
 from projects.models import Project
 
@@ -11,4 +12,11 @@ from .models import Project
 def index(request):
     projects = Project.objects
     return render(request, 'projects/index.html', {'projects':projects})
+
+
+def detail(request, project_id):
+    project_detail = get_object_or_404(Project, pk=project_id)
+    return render(request, 'projects/detail.html', {'project': project_detail})
+
+
 
